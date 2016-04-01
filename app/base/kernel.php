ec0cd2ab03d712b7189dbdf70a1cb06448c40dfd
fengcong <fengcong@shopex.cn>
2016-04-01 10:10:21 +0800

    【创建项目】

---
+++ app/base/kernel.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ *  fcframe
+ *  2016.03.31  by  fengcong
+ */
+
+class kernel{
+
+
+
+
+		static function run(){
+				set_error_handler(array('kernel', 'exception_error_handler'));
+
+
+
+
+
+
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
